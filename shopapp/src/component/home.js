4b46b1bbc0f168e73f96beb42c4fed9ef75c2419
peteralabi <talabi@student.concordia.ab.ca>
2022-07-15 03:48:19 -0600

    factor carousel component

--- shopapp/src/component/home.js
+++ shopapp/src/component/home.js
@@ -3,88 +3,12 @@ import {
   Container,
   Row ,
   DropdownItem,
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption
 } from 'reactstrap';
 import DepartmentCard from './department_card';
 import ShopJumbotron from './shop_jumbotron';
+import PromoCarousel from './carousel';
 
 
-class PromoCarousel extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      activeIndex: 0,
-      items : props.deals || []
-    }
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
-    this.goToIndex = this.goToIndex.bind(this);
-    this.onExiting = this.onExiting.bind(this);
-    this.onExited = this.onExited.bind(this);
-  }
-  onExiting() {
-    this.animating = true;
-  }
-
-  onExited() {
-    this.animating = false;
-  }
-
-  next() {
-    if (this.animating) return;
-    const nextIndex = this.state.activeIndex === this.state.items.length - 1 ? 0 : this.state.activeIndex + 1;
-    this.setState({ activeIndex: nextIndex });
-  }
-
-  previous() {
-    if (this.animating) return;
-    const nextIndex = this.state.activeIndex === 0 ? this.state.items.length - 1 : this.state.activeIndex - 1;
-    this.setState({ activeIndex: nextIndex });
-  }
-
-  goToIndex(newIndex) {
-    if (this.animating) return;
-    this.setState({ activeIndex: newIndex });
-  }
-  render(){
-
-
-    const items = this.state.items;
-    const activeIndex = this.state.activeIndex;
-
-    const slides = items.map((item) => {
-      return (
-        <CarouselItem
-          onExiting={this.onExiting}
-          onExited={this.onExited}
-          key={item.product.images[0].path}
-        >
-          <img src={item.product.images[0].path} alt={item.detail} className='carousel-images'/>
-          <CarouselCaption captionText={item.detail} captionHeader={item.caption} className="carousel-caption"/>
-        </CarouselItem>
-      );
-    });
-
-    return (
-      <Carousel
-        activeIndex={activeIndex}
-        next={this.next}
-        previous={this.previous}
-      >
-        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
-        {slides}
-        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
-        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
-      </Carousel>
-    );
-
-  }
-
-}
 
 export default class Home extends Component {
   constructor(props) {
