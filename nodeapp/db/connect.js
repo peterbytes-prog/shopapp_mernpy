b9bf09021426eafbe97657315948d2117fa45384
peteralabi <talabi@student.concordia.ab.ca>
2022-07-16 06:04:18 -0600

    restructure filesystem, containerized

--- nodeapp/db/connect.js
+++ nodeapp/db/connect.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 
-var mongoDB = mongoose.connect('mongodb://127.0.0.1/shop', {useNewUrlParser:true});
+var mongoDB = mongoose.connect('mongodb://mongo:27017/shop', {useNewUrlParser:true});
+// var mongoDB = mongoose.connect('mongodb://127.0.0.1/shop', {useNewUrlParser:true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, "mongoDB failed to connect"));
 db.on('connected', console.error.bind(console, "mongoDB successsful connected"));
